100c09314e8a9fba9675323f489289537ffc11d3
Darshitsinh1718 <202451041@iiitvadodara.ac.in>
2026-06-14 10:11:17 +0530

    URL are changed

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "https://YOUR_FRONTEND_URL.vercel.app"],
+    "https://trust-os-theta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
